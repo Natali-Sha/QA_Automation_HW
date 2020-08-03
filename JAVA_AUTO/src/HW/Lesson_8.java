8076442acec212c8462528af68182146defd61dc
Natali-Sha <65760731+Natali-Sha@users.noreply.github.com>
2020-08-03 23:38:08 +0300

    Lesson_8 task7

--- JAVA_AUTO/src/HW/Lesson_8.java
+++ JAVA_AUTO/src/HW/Lesson_8.java
@@ -5,6 +5,10 @@ import Lesson_8_1.MyException_Lesson_8_1;
 import Lesson_8_2.Main_Lesson_8_2;
 import Lesson_8_5.ICanThrowException_Lesson_8_5;
 import Lesson_8_6.Number_Lesson_8_6;
+import Lesson_8_7.Lesson_8_7;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Lesson_8 {
     public static void main(String[] args) throws MyException_Lesson_8_1 {
@@ -36,13 +40,20 @@ public class Lesson_8 {
         } catch (NullPointerException e){
             System.out.println("Exception " + e + " was handled");
         }
-
+        System.out.println("------------------");
 
 //            #6
         Number_Lesson_8_6 num = new Number_Lesson_8_6();
         num.readData();
 
+        System.out.println("------------------");
 
+
+//            #7
+        Set<Lesson_8_7> s = new HashSet<>();
+        s.add(new Lesson_8_7("Donald", "Duck"));
+        System.out.println(s.contains(new Lesson_8_7("Donald", "Duck")));
+        System.out.println(s.contains(new Lesson_8_7("Donalds", "Ducks")));
 
 
     }
