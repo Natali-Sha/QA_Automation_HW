663fe6298d41bf24b16d93587582f8926d2b0308
Natali-Sha <65760731+Natali-Sha@users.noreply.github.com>
2020-08-05 20:31:10 +0300

    Lesson_8

--- JAVA_AUTO/src/Lesson_8_1/MyException_Lesson_8_1.java
+++ JAVA_AUTO/src/Lesson_8_1/MyException_Lesson_8_1.java
@@ -4,11 +4,13 @@ package Lesson_8_1;
     public class MyException_Lesson_8_1 extends Exception {
         private int value;
 
-        public MyException_Lesson_8_1(int d) {
+        public MyException_Lesson_8_1(int d)
+        {
             this.value = d;
         }
 
-        public String toString() {
+        public String toString()
+        {
             return "MyException[" + this.value + "]";
         }
 }
--- JAVA_AUTO/src/Lesson_8_2/Main_Lesson_8_2.java
+++ JAVA_AUTO/src/Lesson_8_2/Main_Lesson_8_2.java
@@ -2,19 +2,34 @@ package Lesson_8_2;
 
 public class Main_Lesson_8_2 extends ArithmeticException{
 
-    public boolean divideByZero() {
+    public static int divideByZero() {
         int a = 5;
         int b = 0;
         int result;
-        boolean isDividedByZero = false;
         try {
         result = a / b;
                 } catch (ArithmeticException e) {
         System.out.println(e);
-        isDividedByZero = true;
-    }
-        return isDividedByZero;
+        result = 0;
+        }
+
+        return result;
 
     }
+
+//    public boolean divideByZero() {
+//        int a = 5;
+//        int b = 0;
+//        int result;
+//        boolean isDividedByZero = true;
+//        try {
+//        result = a / b;
+//                } catch (ArithmeticException e) {
+//        System.out.println(e);
+//        isDividedByZero = true;
+//    }
+//        return isDividedByZero;
+//
+//    }
 
 }
